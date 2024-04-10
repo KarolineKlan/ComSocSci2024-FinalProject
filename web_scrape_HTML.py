e2d4cb3627e2fe98b6283631681a03ef887fc2f9
kridk999 <17kkjaer@gmail.com>
2024-04-10 11:33:15 +0200

    loop for pagination

--- web_scrape_HTML.py
+++ web_scrape_HTML.py
@@ -1,45 +1,61 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
-from bs4 import BeautifulSoup
+import pandas as pd
 
 LINK = "https://www.bilkatogo.dk/kategori/"
 categories = ["frugt-og-groent/", "koed-og-fisk/", "mejeri-og-koel/"]
 
+def extract_products(url, category):
+    products_df = pd.DataFrame(columns=['product_id', 'name', 'price', 'link'])
+    page_counter = 0
+    
+    SCRAPE_LINK = f"{url}/{category}"   # Construct the URL for the first case with no page number
+    product_card_containers = ["dummy"]
+    
+    while len(product_card_containers) != 0:
+        print("The length of product_card_containers is: ", len(product_card_containers))
+        print("The page counter is: ", page_counter)
+        
+        # Initialize Edge WebDriver
+        options = webdriver.EdgeOptions()
+        options.add_argument('headless')  # To run Edge in headless mode
+        driver = webdriver.Edge(options=options)
 
-SCRAPE_LINK = LINK + categories[1]
+        # Load the webpage
+        driver.get(SCRAPE_LINK)
 
-# URL of the webpage
-url = SCRAPE_LINK
+        # Wait for a few seconds to ensure JavaScript execution
+        time.sleep(5)
 
-# Initialize Edge WebDriver
-options = webdriver.EdgeOptions()
-options.add_argument('headless')  # To run Edge in headless mode
-driver = webdriver.Edge(options=options)
+        # Get the page source after JavaScript execution
+        page_source = driver.page_source
 
-# Load the webpage
-driver.get(url)
+        # Close the WebDriver
+        driver.quit()
 
-# Wait for a few seconds to ensure JavaScript execution
-time.sleep(5)
+        # Parse the HTML content
+        soup = BeautifulSoup(page_source, 'html.parser')
 
-# Get the page source after JavaScript execution
-page_source = driver.page_source
+        # Find the div element with specific attributes
+        div_element = soup.find_all('div', {'data-v-da5161c2': True, 'data-v-e0535ac4': True})
+        # Find all elements with product-card-container class
+        product_card_containers = soup.find_all('div', {'class' : 'product-card-container'})
+        
+        
+        #JACK INDSÆTTER KODE HER TIL AT EXTRACTE PRODUCT_ID 
+        #TODO:
 
-# Close the WebDriver
-driver.quit()
+        
+        
+        page_counter += 1
+        SCRAPE_LINK = f"{url}/{category}/?page={page_counter}"
+        
+    return products_df
+    
 
-# Parse the HTML content
-soup = BeautifulSoup(page_source, 'html.parser')
 
-# Find the div element with specific attributes
-div_element = soup.find_all('div', {'data-v-da5161c2': True, 'data-v-e0535ac4': True})
-# Find all elements with product-card-container class
-product_card_containers = soup.find_all('div', {'class' : 'product-card-container'})
 
-# You can now process both sets of elements separately
-print(div_element)
-print(product_card_containers)
 
-# Print the div element
-print(div_element)
+
+extract_products(LINK, categories[1])
