66681b7c30f11b3a1d81fed8cd6823b80c810c31
kridk999 <s214609@student.dtu.dk>
2024-04-17 20:13:13 +0200

    added description cleaned data

--- webscrape_descriptions_products_javascript.py
+++ webscrape_descriptions_products_javascript.py
@@ -37,28 +37,35 @@ def get_product_description(link):
     # Parse the HTML content
     soup = BeautifulSoup(page_source, 'html.parser')
     try:
-        description = soup.find('section', {'id' : 'content-description', 'class': 'content'}).text
+        description = soup.find('section', {'id' : 'content-description', 'class': 'content'})
+        a = description.find("h2")
+        if a != None:
+            description = ''.join([str(tag) for tag in reversed(list(description.h2.previous_siblings)) if tag.name != 'h2'])
+            description = description.replace("<br/><br/>","")
+        else:
+            description = description.text
     except AttributeError:
         description = "No description available"
+
     product_descriptions.append(description)
-        
+
     return product_descriptions
 
 
 
 
 if __name__ == "__main__":
-    products = pd.read_csv('data/df_Salling_Products.csv', sep=";")
+    products = pd.read_csv('data/df_clean_data.csv', sep=";")
     product_links = products["link"]
     product_id = products["product_id"]
 
-    
-    for i in tqdm(range(len(pd.read_csv("data/df_Salling_Products_Descriptions.csv", sep=";")), len(product_links), 14)):
 
-        results = Parallel(n_jobs=-2)(delayed(get_product_description)(link) for link in product_links[i:i+14])
+    for i in tqdm(range(len(pd.read_csv('data\df_Salling_Products_Descriptions_CLEANED.csv', sep=";")), len(product_links), 14)):
+
+        results = Parallel(n_jobs=14)(delayed(get_product_description)(link) for link in product_links[i:i+14])
 
         df1['p_id'] = list(product_id[i:i+14])
         df1["descriptions"] = sum(results, [])
-        df1.to_csv('data/df_Salling_Products_Descriptions.csv', sep=';', mode='a', index=False, header=False)
+        df1.to_csv('data/df_Salling_Products_Descriptions_CLEANED.csv', sep=';', mode='a', index=False, header=False)
 
     print(1)
